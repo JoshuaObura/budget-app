b478cbb70e83ecb35b7c2c231c0ee6119d11e09e
jh <oluashegun@googlemail.com>
2020-11-17 12:20:26 +0000

    general functionality sorted

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
 
+  @Input() item: BudgetItem;
+  @Output() xButtonClick: EventEmitter<any> = new EventEmitter<any>()
+  @Output() cardClick: EventEmitter<any> = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onXButtonClick(){
+    //here we want to emit an ecent
+    this.xButtonClick.emit();
+  }
+
+  onCardClick() {
+    this.cardClick.emit();
   }
 
 }
--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
 
 @Component({
   selector: 'app-budget-item-list',
   templateUrl: './budget-item-list.component.html',
   styleUrls: ['./budget-item-list.component.scss']
 })
+
+
 export class BudgetItemListComponent implements OnInit {
-
-  constructor() { }
-
+  
+  @Input() budgetItems: BudgetItem[];
+  @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<UpdateEvent> = new EventEmitter<UpdateEvent>();
+  
+  constructor(public dialog: MatDialog) { }
+  
   ngOnInit(): void {
   }
-
+  
+  onDeleteButtonClicked(item: BudgetItem){
+    this.delete.emit();
+  }
+  
+  onCardClicked(item: BudgetItem) {
+    // display edit modal
+    const dialogRef = this.dialog.open(EditItemModalComponent, {
+      width: '580px',
+      data: item
+    });
+    
+    dialogRef.afterClosed().subscribe(result => {
+      // check for a value
+      if (result) {
+        this.update.emit({
+          old: item,
+          new: result
+        });
+      }
+    })
+  }
 }
+  export interface UpdateEvent {
+    old: BudgetItem;
+    new: BudgetItem;
+  }
